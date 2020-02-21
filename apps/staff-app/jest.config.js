b861965f001b7ec5d509bab8fbe9441aa77444b2
zhirschtirtt <zhirschtritt@gmail.com>
2020-02-20 22:11:13 -0500

    fix: renames

--- apps/staff-app/jest.config.js
+++ apps/staff-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'frontend',
+  name: 'staff-app',
   preset: '../../jest.config.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/frontend'
+  coverageDirectory: '../../coverage/apps/staff-app'
 };
--- apps/staff-app/src/app/app.tsx
+++ apps/staff-app/src/app/app.tsx
@@ -15,7 +15,7 @@ export const App = () => {
     <div className="app">
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to frontend!</h1>
+        <h1>Welcome to staff-app!</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
